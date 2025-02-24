0daf9ae94cc2c4cbde69f9b1b45d8a4d94608852
__None__ <family.sc1lwlral@gmail.com>
2025-02-24 16:50:02 +0900

    Easier to read
    for Secret Staycation // 📜 Made easier for read.

--- SecretStaycation.lua
+++ SecretStaycation.lua
@@ -1,1 +1,9 @@
-game:GetService("Players").LocalPlayer.PlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();game:GetService("StarterPlayer").StarterPlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();game:GetService("Players").LocalPlayer.PlayerGui.Version.TextLabel.Text=game:GetService("Players").LocalPlayer.PlayerGui.Version.TextLabel.Text .. " NoAntiCheatv1";
+--[[
+  Made by NoneScripter
+  Secret Staycation
+  You can use it on your video as showcase, or anything. Just leave a credit.
+]--
+
+game:GetService("Players").LocalPlayer.PlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();
+game:GetService("StarterPlayer").StarterPlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();
+game:GetService("Players").LocalPlayer.PlayerGui.Version.TextLabel.Text=game:GetService("Players").LocalPlayer.PlayerGui.Version.TextLabel.Text .. " NoAntiCheatv1";
