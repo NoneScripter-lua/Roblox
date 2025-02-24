8594f86b8e4093209037903f79b39eb94e086e16
__None__ <family.sc1lwlral@gmail.com>
2025-02-24 16:51:17 +0900

    Easier to read
    for Secret Staycation // 📜 Made easier to read.

--- SecretStaycation.lua
+++ SecretStaycation.lua
@@ -2,7 +2,7 @@
   Made by NoneScripter
   Secret Staycation
   You can use it on your video as showcase, or anything. Just leave a credit.
-]--
+]]--
 
 game:GetService("Players").LocalPlayer.PlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();
 game:GetService("StarterPlayer").StarterPlayerScripts.PlayerSystems.IDontLikeHackers.LocalScript:Destroy();
